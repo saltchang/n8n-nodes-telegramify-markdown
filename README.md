199b25fb7a8e5aad164ee236e958fc2f58396580
Salt Chang <saltchang@outlook.com>
2025-03-30 23:24:43 +0800

    build: add telegramify-markdown node

---
+++ README.md
@@ -0,0 +1,30 @@
+# n8n-nodes-telegramify-markdown
+
+This is an n8n community node. It lets you use Telegramify Markdown in your n8n workflows.
+
+Telegramify Markdown is a library that converts Markdown to Telegram Markdown.
+
+This node is using the node package [telegramify-markdown](https://github.com/skoropadas/telegramify-markdown).
+
+## Installation
+
+Follow the [installation guide](https://docs.n8n.io/integrations/community-nodes/installation/) in the n8n community nodes documentation.
+
+## Operations
+
+- Convert Markdown to Telegram Markdown
+
+## Usage
+
+Just add the node to your workflow and enter the text you want to convert, then output the text to the field you want. The out test should be save to send to Telegram.
+
+## Resources
+
+- [n8n community nodes documentation](https://docs.n8n.io/integrations/community-nodes/)
+- [Telegramify Markdown documentation](https://github.com/saltchang/telegramify-markdown)
+- [node package: telegramify-markdown](https://github.com/skoropadas/telegramify-markdown)
+- [Telegram Markdown](https://core.telegram.org/bots/api#markdown-style)
+
+## License
+
+See the [LICENSE](LICENSE).
---
+++ nodes/TelegramifyMarkdown/TelegramifyMarkdown.node.ts
@@ -0,0 +1,108 @@
+import type {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+import { NodeOperationError } from 'n8n-workflow';
+import telegramifyMarkdown from 'telegramify-markdown';
+
+export class TelegramifyMarkdown implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Telegramify Markdown',
+		name: 'telegramifyMarkdown',
+		group: ['transform', 'utility', 'text', 'markdown', 'telegram'],
+		version: 1,
+		description: 'Telegramify Markdown',
+		defaults: {
+			name: 'Telegramify Markdown',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		properties: [
+			// Node properties which the user gets displayed and
+			// can change on the node.
+			{
+				displayName: 'Text',
+				name: 'text',
+				type: 'string',
+				default: '',
+				placeholder: 'Enter text to convert to Telegram Markdown',
+				description: 'The text to convert to Telegram Markdown',
+			},
+			{
+				displayName: 'Output Field',
+				name: 'outputField',
+				type: 'string',
+				default: 'text',
+				description: 'The field to output the converted text to',
+			},
+			{
+				displayName: 'Escape Mode',
+				name: 'escapeMode',
+				type: 'options',
+				description: 'Unsupported tags strategy as a second argument, which can be one of the following: "escape" - escape unsupported symbols for unsupported tags, "remove" - remove unsupported tags, "keep" - ignore unsupported tags (default)',
+				options: [
+					{
+						name: 'Escape',
+						value: 'escape',
+					},
+					{
+						name: 'Remove',
+						value: 'remove',
+					},
+					{
+						name: 'Keep',
+						value: 'keep',
+					},
+				],
+				default: 'keep',
+			},
+		],
+	};
+
+	// The function below is responsible for actually doing whatever this node
+	// is supposed to do. In this case, we're just appending the `myString` property
+	// with whatever the user has entered.
+	// You can make async calls and use `await`.
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		let item: INodeExecutionData;
+		let text: string;
+
+		// Iterates over all input items and add the key "myString" with the
+		// value the parameter "myString" resolves to.
+		// (This could be a different value for each item in case it contains an expression)
+		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+			try {
+				text = this.getNodeParameter('text', itemIndex, '') as string;
+				const escapeMode = (this.getNodeParameter('escapeMode', itemIndex, 'escape') || 'escape') as 'keep' | 'remove' | 'keep';
+				const outputField = this.getNodeParameter('outputField', itemIndex, 'text') as string;
+				item = items[itemIndex];
+
+				item.json[outputField] = telegramifyMarkdown(text, escapeMode);
+				item.json.escape_mode = escapeMode;
+			} catch (error) {
+				// This node should never fail but we want to showcase how
+				// to handle errors.
+				if (this.continueOnFail()) {
+					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+				} else {
+					// Adding `itemIndex` allows other workflows to handle this error
+					if (error.context) {
+						// If the error thrown already contains the context property,
+						// only append the itemIndex
+						error.context.itemIndex = itemIndex;
+						throw error;
+					}
+					throw new NodeOperationError(this.getNode(), error, {
+						itemIndex,
+					});
+				}
+			}
+		}
+
+		return [items];
+	}
+}
